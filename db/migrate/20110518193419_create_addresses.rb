a7a13742cc4510d181a3f8dd265433b325ea932a
jampow <jampow@97de3b1c-013f-5f49-a299-16bf94642dd6>
2011-06-02 17:54:45 +0000

    organização do arquivo, nenhuma alteração estrutural
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@45 97de3b1c-013f-5f49-a299-16bf94642dd6

--- db/migrate/20110518193419_create_addresses.rb
+++ db/migrate/20110518193419_create_addresses.rb
@@ -1,15 +1,15 @@
 class CreateAddresses < ActiveRecord::Migration
   def self.up
     create_table :addresses do |t|
-      t.string :street
-      t.string :number
-      t.string :complement
-      t.string :neighborhood
-      t.string :city
-      t.string :estate
-      t.string :country
+      t.string  :street
+      t.string  :number
+      t.string  :complement
+      t.string  :neighborhood
+      t.string  :city
+      t.string  :estate
+      t.string  :country
       t.integer :cep
-      t.string :addressable_type
+      t.string  :addressable_type
       t.integer :addressable_id
 
       t.timestamps
