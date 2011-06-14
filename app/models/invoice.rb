a0f675470d4e720b8cbdd0282bfeb9adada32343
jampow <jampow@97de3b1c-013f-5f49-a299-16bf94642dd6>
2011-06-14 01:40:51 +0000

    arrumando relação entre os novos models
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@64 97de3b1c-013f-5f49-a299-16bf94642dd6

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,5 @@
 class Invoice < ActiveRecord::Base
-  belongs_to :client
+  belongs_to :client, :primary_key => "old_id"
+  has_many   :receivables, :primary_key => "invoice_number", :foreign_key => "invoice_number"
 end
 
--- app/models/receivable.rb
+++ app/models/receivable.rb
@@ -1,2 +1,4 @@
 class Receivable < ActiveRecord::Base
+  belongs_to :invoice, :primary_key => "invoice_number", :foreign_key => "invoice_number"
 end
+
