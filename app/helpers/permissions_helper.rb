4fa49791b4e639a8ed8e8db87f9160573ca66551
jampow <jampow@97de3b1c-013f-5f49-a299-16bf94642dd6>
2011-06-24 21:53:57 +0000

    arrumado tela de edição de permições
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@72 97de3b1c-013f-5f49-a299-16bf94642dd6

--- app/helpers/permissions_helper.rb
+++ app/helpers/permissions_helper.rb
@@ -1,5 +1,18 @@
 module PermissionsHelper
 
+
+
+#<div class="w25 radiocheck">
+#	<span class="wrapper">
+#  <span class="title">NCM's</span>
+#    <div class="buttonset">
+#      <input id="permission_ncms_l" name="permission[ncms]" type="radio" value="l" /><label for="permission_ncms_l">Ler</label>
+#      <input checked="checked" id="permission_ncms_e" name="permission[ncms]" type="radio" value="e" /><label for="permission_ncms_e">Escrever</label>
+#      <input id="permission_ncms_s" name="permission[ncms]" type="radio" value="s" /><label for="permission_ncms_s">Sem acesso</label>
+#    </div>
+#	</span>
+#</div>
+
   #Actions New and Edit
   def perm_options(controller, nick, user)
     perm = {"e" => "Escrever", "l" => "Ler", "s" => "Sem acesso"}
@@ -9,6 +22,20 @@ module PermissionsHelper
     perm.each { |n, full| radios += perm_radio(controller, n, user) + "<label for=\"permission_#{controller.to_s}_#{n}\">#{full}</label>" }
 
     s  = "<span class=\"perm_label\">#{nick}</span> #{radios}"
+
+
+    <<-HTML
+    <div class="w25 radiocheck">
+    	<span class="wrapper">
+      <span class="title">#{nick}</span>
+        <div class="buttonset">
+          #{radios}
+        </div>
+    	</span>
+    </div>
+    HTML
+
+
   end
 
   def perm_radio(controller, val, user)
