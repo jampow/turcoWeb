9da9e33bcc68330a1d391d93d1dfe84d5916e2c6
jampow <jampow@97de3b1c-013f-5f49-a299-16bf94642dd6>
2011-11-08 22:04:58 +0000

    Adicionando algumas validações ainda parciais
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@143 97de3b1c-013f-5f49-a299-16bf94642dd6

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -2,6 +2,8 @@ class SalesOrder < ActiveRecord::Base
   belongs_to :client
   has_many   :sales_order_items
 
+  validates_presence_of :client_id
+
   accepts_nested_attributes_for :sales_order_items , :reject_if => proc { |a| a[:product_name].blank? }
 
   before_save :mark_item_for_removal
