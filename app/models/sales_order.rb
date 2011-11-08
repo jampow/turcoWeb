e90e23f0a056aaaa77fb5935681b6b853b73da0c
jampow <jampow@97de3b1c-013f-5f49-a299-16bf94642dd6>
2011-11-08 22:04:51 +0000

    Adicionando exclusão de itens com o nome em branco
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@139 97de3b1c-013f-5f49-a299-16bf94642dd6

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -2,6 +2,14 @@ class SalesOrder < ActiveRecord::Base
   belongs_to :client
   has_many   :sales_order_items
 
-  accepts_nested_attributes_for :sales_order_items #, :reject_if => proc { |a| a[:name].blank? }
+  accepts_nested_attributes_for :sales_order_items , :reject_if => proc { |a| a[:product_name].blank? }
+
+  before_save :mark_contact_for_removal
+
+  def mark_item_for_removal
+    sales_order_items.each do |child|
+      child.mark_for_destruction if child.product_name.blank?
+    end
+  end
 end
 
