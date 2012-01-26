2363be1ac051e2a05dcb25e57df8f0c79923f2f0
nelson <nelson@97de3b1c-013f-5f49-a299-16bf94642dd6>
2012-01-26 00:51:10 +0000

    Calculando porcentagem do total
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@176 97de3b1c-013f-5f49-a299-16bf94642dd6

--- app/models/receivable_cost_division.rb
+++ app/models/receivable_cost_division.rb
@@ -1,5 +1,5 @@
 class ReceivableCostDivision < ActiveRecord::Base
-  attr_accessor :account_plan_name, :cost_center_name
+  attr_accessor :account_plan_name, :cost_center_name, :percent
   belongs_to :account_plan
   belongs_to :cost_center
 end
