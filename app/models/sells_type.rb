88cac5886621fffdbc9f546e0c436e56d515fe60
Gianpaulo Soares <jam_pow@hotmail.com>
2012-02-22 14:55:14 -0200

    adicionandop tipos adicionais de vendas

--- app/models/sells_type.rb
+++ app/models/sells_type.rb
@@ -1,14 +1,18 @@
 class SellsType < ActiveRecord::Base
   has_many :invoices
   validates_presence_of :name
-  
 
   class Type <
     Struct.new(:id, :name)
     VALUES = [
       {:id => 1, :name => 'Venda'},
-      {:id => 2, :name => 'Compra'}
+      {:id => 2, :name => 'Compra'},
+      {:id => 3, :name => 'Simples remessa'},
+      {:id => 4, :name => 'Aparas'},
+      {:id => 5, :name => 'Venda em Manaus'},
+      {:id => 6, :name => 'Beneficiamento com ICMS'}
     ]
+    
     def self.all
       VALUES.map { |v| self.new(v[:id], v[:name]) }
     end
