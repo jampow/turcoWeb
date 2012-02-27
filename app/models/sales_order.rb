7b39d16b9e5b5eb6c6819a8213b9c56f9ea3abf6
Gianpaulo Soares <jam_pow@hotmail.com>
2012-02-27 17:12:58 -0300

    fixando selects nos formulários do pedido de venda

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -2,5 +2,48 @@ class SalesOrder < Order
   belongs_to :client
 
   validates_presence_of :client_id
+  
+  class OrderType < 
+    Struct.new(:id, :name)
+    VALUES = [
+      {:id => 1, :name => 'Venda'},
+      {:id => 2, :name => 'Cotação'}
+    ]
+    def self.all
+      VALUES.map { |v| self.new(v[:id], v[:name]) }
+    end
+
+    def self.to_select
+      VALUES.map { |v| [v[:name], v[:id]] }
+    end
+
+    def self.find(id)
+      h=VALUES.find { |v| v[:id] == id }
+      return nil if h.nil?
+      self.new(h[:id], h[:name])
+    end
+  end
+  
+  class SellType < 
+    Struct.new(:id, :name)
+    VALUES = [
+      {:id => 1, :name => 'Produtos'},
+      {:id => 2, :name => 'Serviços'}
+    ]
+    def self.all
+      VALUES.map { |v| self.new(v[:id], v[:name]) }
+    end
+
+    def self.to_select
+      VALUES.map { |v| [v[:name], v[:id]] }
+    end
+
+    def self.find(id)
+      h=VALUES.find { |v| v[:id] == id }
+      return nil if h.nil?
+      self.new(h[:id], h[:name])
+    end
+  end
+  
 end
 
