f03905cc9e8a763f055e00ddc28f364a49d7bf62
Gianpaulo Soares <jam_pow@hotmail.com>
2012-03-01 14:29:50 -0300

    adicionando relacionamento na NF para a tabela de pedidos

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229194438) do
+ActiveRecord::Schema.define(:version => 20120301172337) do
 
   create_table "account_plans", :force => true do |t|
     t.string   "name",       :limit => 50
@@ -220,6 +220,7 @@ ActiveRecord::Schema.define(:version => 20120229194438) do
     t.boolean  "canceled"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "order_id"
   end
 
   create_table "measure_units", :force => true do |t|
