895ee1206f6d20f1c4701a8aed5baaf53293ece5
jampow <jampow@97de3b1c-013f-5f49-a299-16bf94642dd6>
2012-03-13 18:36:32 +0000

    setando attrs na inicialização da  classe
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@226 97de3b1c-013f-5f49-a299-16bf94642dd6

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,7 +3,12 @@ class OrderItem < ActiveRecord::Base
   attr_accessor :measure_unit
   belongs_to :order
   belongs_to :product
-  
+
+  def after_initialize
+    self.product_name = Product.find(self.product_id).name if self.product_id
+    self.measure_unit = MeasureUnit.find(self.measure_unit_id).measure_unit if self.measure_unit_id
+  end
+
 # Select oi.*
 #      , pr.name
 #      , mu.measure_unit
@@ -12,7 +17,7 @@ class OrderItem < ActiveRecord::Base
 #      measure_units mu On mu.id = oi.measure_unit_id
 # Where oi.id = 1
 
-  named_scope :grid, lambda { |order_id| 
+  named_scope :grid, lambda { |order_id|
     {:select => "oi.*, pr.name as product_name, mu.measure_unit",
      :joins => "oi Join products pr On pr.id = oi.product_id Join measure_units mu On mu.id = oi.measure_unit_id",
      :conditions => ["oi.order_id = ?", order_id],
