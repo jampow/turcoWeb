4cdda8c5f7ec6a2913682cba6cd6c2f6b8061696
nelson <nelson@97de3b1c-013f-5f49-a299-16bf94642dd6>
2012-03-23 13:04:05 +0000

    Settle receivable within billing
    git-svn-id: http://arsky.ddns.com.br:81/svn/turcoWeb@239 97de3b1c-013f-5f49-a299-16bf94642dd6

--- app/models/receivable_billing.rb
+++ app/models/receivable_billing.rb
@@ -1,3 +1,14 @@
 class ReceivableBilling < ActiveRecord::Base
   belongs_to :receivable
+  attr_accessor :settle_receivable
+
+  after_save :settle_receivable!
+
+  private
+  def settle_receivable!
+    if settle_receivable
+      receivable.settled = true
+      receivable.save
+    end
+  end
 end
