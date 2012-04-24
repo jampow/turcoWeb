c9a23222765ab06c1242f3f55433dd5d5f4e3ae5
Gianpaulo Soares <jam_pow@hotmail.com>
2012-04-24 17:44:02 -0300

    Iniciando teste de clientes

--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -2,7 +2,55 @@ require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "will be?" do
+    c = Client.new client
+    assert c.valid?
   end
+
+  def client
+    {
+      :name => "Boa Massa",
+      :nickname => "Boa",
+      :doc => "04.562.324/0001-74",
+      :ie => "",
+      :im => "",
+      :sci => "",
+      :ind_com => "i",
+      :active => true,
+      :email_nfe => "boa@massa.com.br",
+      :observations => "teste bla bla bla",
+      :main_address_attributes => {
+        :street => "Av. Lacerda Franco",
+        :number => "404",
+        :complement => "casa 4",
+        :neighborhood => "Cambuci",
+        :city => "São Paulo",
+        :country => "Brasil",
+        :cep => "01536-000",
+        :estate_id => 26,
+        :city_id => 3831},
+      :billing_address_attributes => {
+        :street => "Av. Lacerda Franco",
+        :number => "404",
+        :complement => "casa 4",
+        :neighborhood => "Cambuci",
+        :city => "São Paulo",
+        :country => "Brasil",
+        :cep => "01536-000",
+        :estate_id => 26,
+        :city_id => 3831},
+      :delivery_address_attributes => {
+        :street => "Av. Lacerda Franco",
+        :number => "404",
+        :complement => "casa 4",
+        :neighborhood => "Cambuci",
+        :city => "São Paulo",
+        :country => "Brasil",
+        :cep => "01536-000",
+        :estate_id => 26,
+        :city_id => 3831},
+      :code => "asd.000.000.123.321"
+    }
+  end
+
 end
