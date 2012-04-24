c37b9cfd32a8180f59829446d5406d4b0c11a010
Gianpaulo Soares <jam_pow@hotmail.com>
2012-04-24 14:50:55 -0300

    antes muito tarde, do que nunca... começando com testes

--- test/unit/cfop_test.rb
+++ test/unit/cfop_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class CfopTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "should not save the CFOP with out a number" do
+    cfop = Cfop.new
+    assert !cfop.save
+  end
+
+  test "should be invalid with a big name" do
+    cfop = Cfop.new :number => 2000,
+                    :name => "aqui vai um nome absurdo com mais de 40 caracteres"
+    assert !cfop.valid?, "Nome muito grande"
+  end
+
+  test "should number is between 1100 and 8000" do
+    cfop = Cfop.new :number => 1000,
+                    :name => "cfop inválida"
+    assert !cfop.valid?, "Número fora do intervalo"
   end
 end
