c5e32e0b6c863564cfcba71defdc30624a8f399f
Gianpaulo Soares <jam_pow@hotmail.com>
2012-05-04 12:25:48 -0300

    Adicionando validações na criação do contas a receber

--- app/models/receivable.rb
+++ app/models/receivable.rb
@@ -1,4 +1,43 @@
 class Receivable < ActiveRecord::Base
+  validates_presence_of :client_id
+  validates_presence_of :issue_date
+  validates_presence_of :due_date
+  validates_presence_of :value
+  validate :due_greater_than_issue_date
+
+  def due_greater_than_issue_date
+    unless due_date.nil? || issue_date.nil?
+      self.errors.add("due_date", " tem que ser posterior ou igual a data de emissão") if due_date < issue_date.to_date
+    end
+  end
+
+  belongs_to :invoice, :primary_key => "invoice_number", :foreign_key => "invoice_number"
+  belongs_to :client
+  belongs_to :bank_account, :foreign_key => "account_id"
+
+  has_many :receivable_cost_divisions
+  has_many :billings, :class_name => 'ReceivableBilling'
+
+  accepts_nested_attributes_for :receivable_cost_divisions, :allow_destroy => true, :reject_if => proc { |attributes| attributes['value'].blank? }
+
+  attr_accessor :client_name
+
+  # def after_initialize
+  #   self.client_name = Client.find(self.client_id, :select => 'name').name if self.client_id
+  # end
+
+# Select rec.id
+#      , cli.name as client
+#      , rec.invoice_number
+#      , rec.due_date
+#      , rec.value
+#      , rec.settled
+# From receivables rec
+# Join clients     cli On cli.id = rec.client_id
+
+  named_scope :grid, :select => "rec.id, cli.name as cli, rec.invoice_number, rec.due_date, rec.value, rec.settled",
+                     :joins  => "rec Join clients cli On cli.id = rec.client_id"
+
   class DocumentKind <
     Struct.new(:id, :kind)
     VALUES = [
@@ -99,31 +138,5 @@ class Receivable < ActiveRecord::Base
     end
   end
 
-  belongs_to :invoice, :primary_key => "invoice_number", :foreign_key => "invoice_number"
-  belongs_to :client
-  belongs_to :bank_account, :foreign_key => "account_id"
-
-  has_many :receivable_cost_divisions
-  has_many :billings, :class_name => 'ReceivableBilling'
-
-  accepts_nested_attributes_for :receivable_cost_divisions, :allow_destroy => true, :reject_if => proc { |attributes| attributes['value'].blank? }
-
-  attr_accessor :client_name
-
-  # def after_initialize
-  #   self.client_name = Client.find(self.client_id, :select => 'name').name if self.client_id
-  # end
-
-# Select rec.id
-#      , cli.name as client
-#      , rec.invoice_number
-#      , rec.due_date
-#      , rec.value
-#      , rec.settled
-# From receivables rec
-# Join clients     cli On cli.id = rec.client_id
-
-  named_scope :grid, :select => "rec.id, cli.name as cli, rec.invoice_number, rec.due_date, rec.value, rec.settled",
-                     :joins  => "rec Join clients cli On cli.id = rec.client_id"
 end
 
