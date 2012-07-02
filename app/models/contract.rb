631667cbf76079076c63f0cc47997949e27ee90a
Gianpaulo M. Soares <jam_pow@hotmail.com>
2012-07-02 20:23:32 -0300

    melhorando select com criação de campo para ordenação das cláusulas

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -19,10 +19,11 @@ class Contract < ActiveRecord::Base
                :empresa_endereco      => "@ent.address.to_s"}
 
 # Select c1.id
-#      , Concat(IfNull(Cast(c4.`order` As Char),'')
-#             , IfNull(Cast(c3.`order` As Char),'')
-#             , IfNull(Cast(c2.`order` As Char),'')
-#             , IfNull(Cast(c1.`order` As Char),'')) As ordenator
+#      , RPad(
+#        Concat(IfNull(Concat(LPad(c4.`order`, 3, '0'),'.'),'')
+#              ,IfNull(Concat(LPad(c3.`order`, 3, '0'),'.'),'')
+#              ,IfNull(Concat(LPad(c2.`order`, 3, '0'),'.'),'')
+#              ,IfNull(LPad(c1.`order`, 3, '0'),'')), 15, '.000') As ordenator
 #      , SubString(c1.text, 1, 200) as text
 # From contracts c1
 # Left Join contracts c2 On c2.id = c1.father_id
@@ -30,7 +31,7 @@ class Contract < ActiveRecord::Base
 # Left Join contracts c4 On c4.id = c3.father_id
 # Order by ordenator;
 
-  named_scope :grid, :select => "c1.id, Concat('.',IfNull(Cast(c4.`order` As Char),''), IfNull(Cast(c3.`order` As Char),''), IfNull(Cast(c2.`order` As Char),''), IfNull(Cast(c1.`order` As Char),'')) As ordenator, SubString(c1.text, 1, 300) as text ",
+  named_scope :grid, :select => "c1.id, RPad(Concat(IfNull(Concat(LPad(c4.`order`, 3, '0'),'.'),''), IfNull(Concat(LPad(c3.`order`, 3, '0'),'.'),''), IfNull(Concat(LPad(c2.`order`, 3, '0'),'.'),''), IfNull(LPad(c1.`order`, 3, '0'),'')), 15, '.000') As ordenator, SubString(c1.text, 1, 200) as text",
                      :joins  => "c1 Left Join contracts c2 On c2.id = c1.father_id Left Join contracts c3 On c3.id = c2.father_id Left Join contracts c4 On c4.id = c3.father_id",
                      :order  => "ordenator"
 
