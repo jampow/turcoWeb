732435c64f3730386c3cc254565616123ab1dec0
Gianpaulo M. Soares <jam_pow@hotmail.com>
2012-08-11 13:26:04 -0300

    arrumando erro de grafia

--- app/helpers/financial_report_helper.rb
+++ app/helpers/financial_report_helper.rb
@@ -1,7 +1,7 @@
 module FinancialReportHelper
   def menu
     s  = link_to "Demonstrativo"  , "financial_report/cash_flow_filter"   , :class => "button"
-    s += link_to "Despezas"       , "financial_report/expenditure_filter" , :class => "button"
+    s += link_to "Despesas"       , "financial_report/expenditure_filter" , :class => "button"
     s += link_to "Receitas"       , "financial_report/sales_filter"       , :class => "button"
     s += link_to "Plano de contas", "financial_report/account_plan_filter", :class => "button"
   end
