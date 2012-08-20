2bca061426a131d1ccc03ae45d487003acd555c1
Gianpaulo M. Soares <jam_pow@hotmail.com>
2012-08-20 11:40:22 -0300

    Arrumando bug de cálculo com nulo no saldo das transações bancárias

--- app/models/bank_account_transaction.rb
+++ app/models/bank_account_transaction.rb
@@ -5,7 +5,7 @@ class BankAccountTransaction < ActiveRecord::Base
   belongs_to :bank_account
 
 #   Select ba.*
-#        , (Select Sum(sba.credit) - Sum(sba.debit)
+#        , (Select IfNull(Sum(sba.credit),0.00) - IfNull(Sum(sba.debit),0.00)
 #             From bank_account_transactions sba
 #            Where sba.date <= ba.date
 #              And sba.id <= ba.id
@@ -15,7 +15,7 @@ class BankAccountTransaction < ActiveRecord::Base
 # Order by ba.date
 
   named_scope :transactions, lambda { |bank_account_id| {
-    :select     => " ba.*, (Select Sum(sba.credit) - Sum(sba.debit) From bank_account_transactions sba Where sba.date <= ba.date And sba.id <= ba.id And sba.bank_account_id = ba.bank_account_id) As balance",
+    :select     => " ba.*, (Select IfNull(Sum(sba.credit),0.00) - IfNull(Sum(sba.debit),0.00) From bank_account_transactions sba Where sba.date <= ba.date And sba.id <= ba.id And sba.bank_account_id = ba.bank_account_id) As balance",
     :joins      => "ba",
     :conditions => ["ba.bank_account_id = ?", bank_account_id],
     :order      => "ba.date"}
