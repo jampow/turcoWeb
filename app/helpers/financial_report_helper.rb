b1a4a2434f2caee3196cfde188561cca905d82b2
Gianpaulo M. Soares <jam_pow@hotmail.com>
2012-10-12 18:06:45 -0300

    renomeando relatórios financeiros

--- app/helpers/financial_report_helper.rb
+++ app/helpers/financial_report_helper.rb
@@ -1,8 +1,8 @@
 module FinancialReportHelper
   def menu
-    s  = link_to "Demonstrativo"  , "financial_report/cash_flow_filter"   , :class => "button"
+    s  = link_to "Fluxo de caixa" , "financial_report/cash_flow_filter"   , :class => "button"
     s += link_to "Despesas"       , "financial_report/expenditure_filter" , :class => "button"
     s += link_to "Receitas"       , "financial_report/sales_filter"       , :class => "button"
-    s += link_to "Plano de contas", "financial_report/account_plan_filter", :class => "button"
+    s += link_to "Balancete"      , "financial_report/account_plan_filter", :class => "button"
   end
 end
