4f8c05a9fff241fc2608888edcf350a970c40d6a
Gianpaulo M. Soares <jam_pow@hotmail.com>
2013-02-03 17:44:37 -0200

    apagando tabelas antes de criar a view, por que elas agora são tabelas e não mais views?

--- db/migrate/20130203183119_redefine_views.rb
+++ db/migrate/20130203183119_redefine_views.rb
@@ -7,21 +7,27 @@ class RedefineViews < ActiveRecord::Migration
     # adicionado "OR settled is null" na cláusula Where
 
     execute "Drop table if exists VwCashFlow"
-    execute "CREATE OR REPLACE VIEW `VwCashFlow` AS select `cf`.`date` AS `date`,sum(`cf`.`rec`) AS `rec`,sum(`cf`.`pay`) AS `pay`,(sum(`cf`.`rec`) - sum(`cf`.`pay`)) AS `total` from `VwCashFlowUnion` `cf` group by `cf`.`date` order by `cf`.`date`"
-    execute "Drop table if exists VwCashFlowPayables"
-    execute "CREATE OR REPLACE VIEW `VwCashFlowPayables` AS select `sub`.`due_date` AS `date`,0.00 AS `rec`,0.00 AS `recd`,sum(`sub`.`payable`) AS `pay`,0.00 AS `payd` from `VwCashFlowPayablesPre` `sub` group by `sub`.`due_date`"
-    execute "Drop table if exists VwCashFlowPayablesPre"
-    execute "CREATE OR REPLACE VIEW `VwCashFlowPayablesPre` AS select `pay`.`id` AS `id`,cast(`pay`.`due_date` as date) AS `due_date`,(`pay`.`value` - sum(ifnull(`pab`.`value`,0.00))) AS `payable` from (`payables` `pay` left join `payable_billings` `pab` on((`pab`.`payable_id` = `pay`.`id`))) where ((ifnull(`pab`.`value`,0.00) < `pay`.`value`) and (`pay`.`settled` is false or `pay`.`settled` is null)) group by `pay`.`id`"
-    execute "Drop table if exists VwCashFlowPayd"
-    execute "CREATE OR REPLACE VIEW `VwCashFlowPayd` AS select cast(`payable_billings`.`received_at` as date) AS `date`,0.00 AS `rec`,0.00 AS `recd`,0.00 AS `pay`,sum(`payable_billings`.`value`) AS `payd` from `payable_billings` group by `payable_billings`.`received_at`"
-    execute "Drop table if exists VwCashFlowReceivables"
-    execute "CREATE OR REPLACE VIEW `VwCashFlowReceivables` AS select `sub`.`due_date` AS `date`,sum(`sub`.`receivable`) AS `rec`,0.00 AS `recd`,0.00 AS `pay`,0.00 AS `payd` from `VwCashFlowReceivablesPre` `sub` group by `sub`.`due_date`"
-    execute "Drop table if exists VwCashFlowReceivablesPre"
-    execute "CREATE OR REPLACE VIEW `VwCashFlowReceivablesPre` AS select `rec`.`id` AS `id`,cast(`rec`.`due_date` as date) AS `due_date`,(`rec`.`value` - sum(ifnull(`reb`.`value`,0.00))) AS `receivable` from (`receivables` `rec` left join `receivable_billings` `reb` on((`reb`.`receivable_id` = `rec`.`id`))) where ((ifnull(`reb`.`value`,0.00) < `rec`.`value`) and (`rec`.`settled` is false or `rec`.`settled` is null)) group by `rec`.`id`"
-    execute "Drop table if exists VwCashFlowReceived"
-    execute "CREATE OR REPLACE VIEW `VwCashFlowReceived` AS select cast(`receivable_billings`.`received_at` as date) AS `date`,0.00 AS `rec`,sum(`receivable_billings`.`value`) AS `recd`,0.00 AS `pay`,0.00 AS `payd` from `receivable_billings` group by `receivable_billings`.`received_at`"
     execute "Drop table if exists VwCashFlowUnion"
+
+    execute "Drop table if exists VwCashFlowReceived"
+    execute "Drop table if exists VwCashFlowReceivables"
+    execute "Drop table if exists VwCashFlowReceivablesPre"
+
+    execute "Drop table if exists VwCashFlowPayd"
+    execute "Drop table if exists VwCashFlowPayables"
+    execute "Drop table if exists VwCashFlowPayablesPre"
+
+    execute "CREATE OR REPLACE VIEW `VwCashFlowPayablesPre` AS select `pay`.`id` AS `id`,cast(`pay`.`due_date` as date) AS `due_date`,(`pay`.`value` - sum(ifnull(`pab`.`value`,0.00))) AS `payable` from (`payables` `pay` left join `payable_billings` `pab` on((`pab`.`payable_id` = `pay`.`id`))) where ((ifnull(`pab`.`value`,0.00) < `pay`.`value`) and (`pay`.`settled` is false or `pay`.`settled` is null)) group by `pay`.`id`"
+    execute "CREATE OR REPLACE VIEW `VwCashFlowPayables` AS select `sub`.`due_date` AS `date`,0.00 AS `rec`,0.00 AS `recd`,sum(`sub`.`payable`) AS `pay`,0.00 AS `payd` from `VwCashFlowPayablesPre` `sub` group by `sub`.`due_date`"
+    execute "CREATE OR REPLACE VIEW `VwCashFlowPayd` AS select cast(`payable_billings`.`received_at` as date) AS `date`,0.00 AS `rec`,0.00 AS `recd`,0.00 AS `pay`,sum(`payable_billings`.`value`) AS `payd` from `payable_billings` group by `payable_billings`.`received_at`"
+
+    execute "CREATE OR REPLACE VIEW `VwCashFlowReceivablesPre` AS select `rec`.`id` AS `id`,cast(`rec`.`due_date` as date) AS `due_date`,(`rec`.`value` - sum(ifnull(`reb`.`value`,0.00))) AS `receivable` from (`receivables` `rec` left join `receivable_billings` `reb` on((`reb`.`receivable_id` = `rec`.`id`))) where ((ifnull(`reb`.`value`,0.00) < `rec`.`value`) and (`rec`.`settled` is false or `rec`.`settled` is null)) group by `rec`.`id`"
+    execute "CREATE OR REPLACE VIEW `VwCashFlowReceivables` AS select `sub`.`due_date` AS `date`,sum(`sub`.`receivable`) AS `rec`,0.00 AS `recd`,0.00 AS `pay`,0.00 AS `payd` from `VwCashFlowReceivablesPre` `sub` group by `sub`.`due_date`"
+    execute "CREATE OR REPLACE VIEW `VwCashFlowReceived` AS select cast(`receivable_billings`.`received_at` as date) AS `date`,0.00 AS `rec`,sum(`receivable_billings`.`value`) AS `recd`,0.00 AS `pay`,0.00 AS `payd` from `receivable_billings` group by `receivable_billings`.`received_at`"
+
     execute "CREATE OR REPLACE VIEW `VwCashFlowUnion` AS select `VwCashFlowReceivables`.`date` AS `date`,`VwCashFlowReceivables`.`rec` AS `rec`,`VwCashFlowReceivables`.`recd` AS `recd`,`VwCashFlowReceivables`.`pay` AS `pay`,`VwCashFlowReceivables`.`payd` AS `payd` from `VwCashFlowReceivables` union all select `VwCashFlowReceived`.`date` AS `date`,`VwCashFlowReceived`.`rec` AS `rec`,`VwCashFlowReceived`.`recd` AS `recd`,`VwCashFlowReceived`.`pay` AS `pay`,`VwCashFlowReceived`.`payd` AS `payd` from `VwCashFlowReceived` union all select `VwCashFlowPayables`.`date` AS `date`,`VwCashFlowPayables`.`rec` AS `rec`,`VwCashFlowPayables`.`recd` AS `recd`,`VwCashFlowPayables`.`pay` AS `pay`,`VwCashFlowPayables`.`payd` AS `payd` from `VwCashFlowPayables` union all select `VwCashFlowPayd`.`date` AS `date`,`VwCashFlowPayd`.`rec` AS `rec`,`VwCashFlowPayd`.`recd` AS `recd`,`VwCashFlowPayd`.`pay` AS `pay`,`VwCashFlowPayd`.`payd` AS `payd` from `VwCashFlowPayd`"
+
+    execute "CREATE OR REPLACE VIEW `VwCashFlow` AS select `cf`.`date` AS `date`,sum(`cf`.`rec`) AS `rec`,sum(`cf`.`pay`) AS `pay`,(sum(`cf`.`rec`) - sum(`cf`.`pay`)) AS `total` from `VwCashFlowUnion` `cf` group by `cf`.`date` order by `cf`.`date`"
 
   end
 
